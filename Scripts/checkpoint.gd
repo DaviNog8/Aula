348bb674fb3442a6b0532aaeca5a610cb25b278a
Davi <davinognat@yahoo.com>
2024-11-30 11:51:21 -0300

    inicio

---
+++ Scripts/checkpoint.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+
+
+func _ready() -> void:
+	var animatedSprite = get_child(0) as AnimatedSprite2D
+	animatedSprite.animation = "NoFlag"
+	animatedSprite.play()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		var animatedSprite = get_child(0) as AnimatedSprite2D
+		animatedSprite.animation = "FlagOut"
+		animatedSprite.play()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	var animatedSprite = get_child(0) as AnimatedSprite2D
+	animatedSprite.animation = "FlagIdle"
+	animatedSprite.play()
---
+++ Scripts/player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var player 
+
+func _ready() -> void:
+	player = get_child(0)
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_right"):
+		player.animation = "Run"
+		player.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		player.animation = "Run"
+		player.flip_h = true
+	else:
+		player.animation = "Idle"
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
